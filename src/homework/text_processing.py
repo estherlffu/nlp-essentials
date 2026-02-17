85f3111b7f38b16ca4da66c2a70e30520811ce98
estherlffu <esther.fu@emory.edu>
2026-02-16 22:35:02 -0500

    Progress -- Feb. 16, 2026 22:34

--- src/homework/text_processing.py
+++ src/homework/text_processing.py
@@ -1,1 +1,34 @@
+import re
 
+def chronicles_of_naria(filename):
+
+    filename = 'dat/chronicles_of_narnia.txt'
+
+    books = {} # dict to be returned
+    book_track = None # to keep track of current book
+    chapter_track = None # to keep track of current chapter
+
+    # Identify books & chapters
+    book_identify = re.compile(r'^(.*)\s+\(\s(\d{4})\s\)$') # Before ( 4 digit number )
+    chapter_identify = re.compile(r'^CHAPTER\s([IVX]+)$') # Line after CHAPTER _
+
+    # Convert roman numeral to integer
+    def chapter_number(rn):
+        roman = {'I': 1, 'V': 5, 'X':10} # All books have less than 20 chapters
+        output = 0
+        previous = 0
+
+        # traverse input from end
+        for i in reversed(rn):
+            t = roman[i] # use current char (temp)
+            if t < previous: # smaller before larger ex. IV = 4
+                output -= t
+            else: # else ex. VI = 5
+                output += t
+            previous = t
+
+        return output
+
+    # To isolate lines
+    with open(filename, 'r') as f: # read input file
+        lines = f.readlines()
